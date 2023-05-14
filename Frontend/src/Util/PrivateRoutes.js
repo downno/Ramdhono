2144761a1a6355cfc9469fe9f08f8ec7f9a1adf6
Ramdhono <127644352+downno@users.noreply.github.com>
2023-05-14 15:29:35 +0700

    PrivateRoutes.js

--- Frontend/src/Util/PrivateRoutes.js
+++ Frontend/src/Util/PrivateRoutes.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function PrivateRoutes () {
-        //Dummy autentication
+       
     let authentication = {
-        token : "", //biasanya ini didapatkan dari data backend
+        token : "", 
     };
 
     return authentication.token ? <Outlet/> : <Navigate to="/login"/>
